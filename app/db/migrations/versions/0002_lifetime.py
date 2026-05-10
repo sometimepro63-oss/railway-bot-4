ff50efe686e54844f7cf76213195badb51406844
sometimepro63-oss <sometimepro63@gmail.com>
2026-05-10 13:25:53 +0400

    Shorten Alembic lifetime migration revision

--- app/db/migrations/versions/0002_lifetime.py
+++ app/db/migrations/versions/0002_lifetime.py
@@ -4,7 +4,7 @@ from alembic import op
 import sqlalchemy as sa
 
 
-revision = "0002_subscription_expires_nullable"
+revision = "0002_lifetime"
 down_revision = "0001_init"
 branch_labels = None
 depends_on = None
